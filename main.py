cf2ff237285cfee02839193853655e35e2425c89
Ayush Aman <nympseudo259@gmail.com>
2024-02-28 20:56:19 +0530

    Step 0 implemented of the ccwc

--- main.py
+++ main.py
@@ -1,4 +1,20 @@
 import click
 
+
 @click.command()
-@click.option('')
+@click.option('-c', is_flag=True, help="Count the number of Bytes in the FILE.")
+@click.argument('file', type=click.Path())
+def pyc(c, file):
+
+    """
+
+        Print the number of Bytes in the FILE.
+
+    """
+
+    if c:
+        with open(file, 'rb') as f:
+            click.echo(f'{len(f.read())} {file}')
+
+if __name__ == '__main__':
+    pyc()
